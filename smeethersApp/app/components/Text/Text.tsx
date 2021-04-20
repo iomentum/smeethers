01a428f63194ecd2cd23d128f4a7b603c94c3178
Rabire <rabireh@outlook.fr>
2021-04-20 17:22:16 +0200

    section component slpit + text compo

---
+++ smeethersApp/app/components/Text/Text.tsx
@@ -0,0 +1,34 @@
+import React, {FC} from 'react';
+import {Text as ReactNativeText} from 'react-native';
+import merge from 'lodash/merge';
+import {presets} from './Text.presets';
+import {TextProps} from './Text.props';
+import {translate} from '../../i18n';
+
+export const Text: FC<TextProps> = ({
+  preset = 'default',
+  tx,
+  txOptions,
+  text,
+  children,
+  style: styleOverride,
+  ...rest
+}) => {
+  // figure out which content to use
+  const i18nText = tx && translate(tx, txOptions);
+  const content = i18nText || text || children;
+
+  const style = merge({}, presets[preset] || presets.default);
+
+  if (Array.isArray(styleOverride)) {
+    merge(style, ...styleOverride);
+  } else if (styleOverride) {
+    merge(style, styleOverride);
+  }
+
+  return (
+    <ReactNativeText {...rest} style={style}>
+      {content}
+    </ReactNativeText>
+  );
+};
