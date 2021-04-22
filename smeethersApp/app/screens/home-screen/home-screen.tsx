fd06fd55644e6aced5105bfa18b38951db3dcc81
Rabire <rabireh@outlook.fr>
2021-04-22 21:48:23 +0200

    home useCallback

--- smeethersApp/app/screens/home-screen/home-screen.tsx
+++ smeethersApp/app/screens/home-screen/home-screen.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import {TouchableOpacity} from 'react-native';
 import Screen from '../../components/Screen/Screen';
 
 import {HeaderBox, PendingReplyText} from './home-screen.styles';
@@ -8,12 +9,24 @@ import CharacterLogo from '../../components/Svg/CharacterLogo';
 import Add from '../../components/Svg/Add';
 
 const HomeScreen = () => {
+  const openMenu = useCallback(() => {
+    console.log('openm enu');
+  }, []);
+
+  const addMeeting = useCallback(() => {
+    console.log('go to addMeeting screen');
+  }, []);
+
   return (
     <Screen>
       <HeaderBox>
-        <BurgerMenu />
+        <TouchableOpacity onPress={openMenu}>
+          <BurgerMenu />
+        </TouchableOpacity>
         <CharacterLogo />
-        <Add />
+        <TouchableOpacity onPress={addMeeting}>
+          <Add />
+        </TouchableOpacity>
       </HeaderBox>
 
       <PendingReplyText text="Monsieur Rabire, vous avez 2 demandes en attente de réponse" />
