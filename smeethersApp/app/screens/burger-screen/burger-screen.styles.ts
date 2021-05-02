0602e17d3feb74d9e3045cc0c4e393113ab6f161
Kaayn <leo.dimartinopro@gmail>
2021-05-02 09:05:38 +0200

    up

--- smeethersApp/app/screens/burger-screen/burger-screen.styles.ts
+++
@@ -1,4 +0,0 @@
-import styled from 'styled-components/native';
-import Text from '../../components/Text/Text';
-import {typography} from '../../theme';
-import {vw} from '../../utils/viewport-units';
