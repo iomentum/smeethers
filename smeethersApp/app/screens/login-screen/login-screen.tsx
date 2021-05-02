8eafaad498767c367df903234a66f7a5b014b505
Yann <45734971+YannBonaudo@users.noreply.github.com>
2021-05-02 18:31:29 +0200

    lgogin screen (#1)

--- smeethersApp/app/screens/login-screen/login-screen.tsx
+++ smeethersApp/app/screens/login-screen/login-screen.tsx
@@ -3,6 +3,19 @@ import {useNavigation} from '@react-navigation/native';
 import {saveToken} from '../../utils/storage';
 import Text from '../../components/Text/Text';
 import Screen from '../../components/Screen/Screen';
+import {color} from '../../theme';
+import SmeethersLogo from '../../components/Svg/SmeethersLogo';
+import LogoEyesUp from '../../components/Svg/LogoEyesUp';
+import {
+  LogoBox,
+  SloganText,
+  LoginTextInput,
+  CredentialsBox,
+  GlobalView,
+  ForgottenPasswordText,
+  FooterLogoView,
+} from './login-screen.styles';
+import {translate} from '../../i18n';
 
 const LoginScreen = () => {
   saveToken('hehe token');
@@ -16,8 +29,30 @@ const LoginScreen = () => {
 
   return (
     <Screen>
-      <Text text="LoginScreen" />
-      <Text text="connect" onPress={() => navigation.navigate('home')} />
+      <GlobalView>
+        <LogoBox>
+          <SmeethersLogo />
+          <SloganText tx="common.slogan" />
+        </LogoBox>
+        <CredentialsBox>
+          <LoginTextInput
+            placeholder={translate('login.email')}
+            placeholderTextColor={color.font}
+          />
+          <LoginTextInput
+            placeholder={translate('login.password')}
+            placeholderTextColor={color.font}
+          />
+          <ForgottenPasswordText tx="login.forgottenPassword" />
+          <ForgottenPasswordText
+            text="se connecter"
+            onPress={() => navigation.navigate('home')}
+          />
+        </CredentialsBox>
+      </GlobalView>
+      <FooterLogoView>
+        <LogoEyesUp />
+      </FooterLogoView>
     </Screen>
   );
 };
